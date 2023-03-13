593718eaf6050c6d33ce97c592da9099136221ca
INleanRe <yanawahyu37@gmail.com>
2023-03-13 17:38:54 +0700

    celeng fridei

---
+++ dailyceleng.js
@@ -0,0 +1,237 @@
+const person = [
+    {
+      "_id": "640b3d996470eb2e6aed8dfa",
+      "isActive": true,
+      "age": 28,
+      "eyeColor": "brown",
+      "name": "Lakeisha Ramsey",
+      "gender": "female",
+      "address": "128 Conduit Boulevard, Caroleen, Arkansas, 4630",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "640b3d99fe71d4348d47303f",
+      "isActive": false,
+      "age": 32,
+      "eyeColor": "blue",
+      "name": "Velez Freeman",
+      "gender": "male",
+      "address": "967 Folsom Place, Allendale, Puerto Rico, 8034",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "640b3d99ff98b5c0a8294f9f",
+      "isActive": false,
+      "age": 24,
+      "eyeColor": "brown",
+      "name": "Stone Riddle",
+      "gender": "male",
+      "address": "319 Furman Street, Greenock, Delaware, 1045",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "640b3d99be0dc952c429db54",
+      "isActive": true,
+      "age": 28,
+      "eyeColor": "green",
+      "name": "Kristi Nguyen",
+      "gender": "female",
+      "address": "942 Cortelyou Road, Detroit, Iowa, 6246",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "640b3d99f5c6be9abdaa0c7c",
+      "isActive": true,
+      "age": 20,
+      "eyeColor": "green",
+      "name": "Bette Ratliff",
+      "gender": "female",
+      "address": "289 School Lane, Keyport, Kentucky, 8374",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "640b3d998a4bb335616e9e19",
+      "isActive": true,
+      "age": 23,
+      "eyeColor": "green",
+      "name": "Blackwell Humphrey",
+      "gender": "male",
+      "address": "320 Guider Avenue, Roderfield, Guam, 704",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "640b3d99c9bd98ec4c0becb8",
+      "isActive": true,
+      "age": 38,
+      "eyeColor": "blue",
+      "name": "Victoria Jefferson",
+      "gender": "female",
+      "address": "959 Eckford Street, Cascades, Florida, 7096",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "640b3d9980ead1dee4871b3e",
+      "isActive": false,
+      "age": 33,
+      "eyeColor": "blue",
+      "name": "Gentry Livingston",
+      "gender": "male",
+      "address": "267 Duryea Court, Springhill, Northern Mariana Islands, 5440",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "640b3d99e5acd3969be86d9a",
+      "isActive": true,
+      "age": 24,
+      "eyeColor": "blue",
+      "name": "Jeanne Graham",
+      "gender": "female",
+      "address": "487 Times Placez, Jackpot, South Dakota, 5559",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "640b3d992ae04698a8771b19",
+      "isActive": true,
+      "age": 24,
+      "eyeColor": "brown",
+      "name": "Drake Reilly",
+      "gender": "male",
+      "address": "533 Chestnut Avenue, Sena, Indiana, 7841",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "640b3d990ad3142c5acc71e6",
+      "isActive": true,
+      "age": 26,
+      "eyeColor": "blue",
+      "name": "Pam Velazquez",
+      "gender": "female",
+      "address": "443 Veranda Place, Coldiron, Nebraska, 3470",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "640b3d99905f4e19f5df4b47",
+      "isActive": true,
+      "age": 32,
+      "eyeColor": "green",
+      "name": "Patel Clayton",
+      "gender": "male",
+      "address": "861 Love Lane, Avalon, Kansas, 9230",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "640b3d99c59582dc53bdc7ba",
+      "isActive": true,
+      "age": 21,
+      "eyeColor": "green",
+      "name": "Lucille Gross",
+      "gender": "female",
+      "address": "240 Kimball Street, Austinburg, Texas, 8213",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "640b3d99a4e188a4f788e689",
+      "isActive": false,
+      "age": 33,
+      "eyeColor": "brown",
+      "name": "Jaclyn Clements",
+      "gender": "female",
+      "address": "452 Drew Street, Wilmington, American Samoa, 431",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "640b3d99e3b633236c288cf5",
+      "isActive": true,
+      "age": 40,
+      "eyeColor": "brown",
+      "name": "Pacheco Hull",
+      "gender": "male",
+      "address": "379 Dare Court, Fairfield, Maryland, 3355",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "640b3d997c6b1e6a4944d4e8",
+      "isActive": false,
+      "age": 34,
+      "eyeColor": "green",
+      "name": "Shawna Eaton",
+      "gender": "female",
+      "address": "190 Kenilworth Place, Campo, Illinois, 8885",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "640b3d998ba65f885c1b77df",
+      "isActive": true,
+      "age": 32,
+      "eyeColor": "green",
+      "name": "Lopez Turner",
+      "gender": "male",
+      "address": "897 Dooley Street, Sunriver, Mississippi, 5796",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "640b3d990b5a13e83701866e",
+      "isActive": true,
+      "age": 22,
+      "eyeColor": "brown",
+      "name": "Carr Hurley",
+      "gender": "male",
+      "address": "807 Madison Place, Makena, Vermont, 4603",
+      "favoriteFruit": "strawberry"
+    }
+  ]
+
+// person suka apple warna mata blue
+for (i=0; i<person.length; i++){
+  let arr = []
+  if(person[i].favoriteFruit === "apple" & person[i].eyeColor === "blue"){
+      // console.log(person[i])
+  }
+}
+// berapa person age > 30
+let a = 0
+for(i = 0; i<person.length; i++){
+  if(person[i].age > 30){
+    a += 1
+  }
+}
+// console.log(a)
+// Person isactive female
+for (i=0; i<person.length; i++){
+  let arr = []
+  if(person[i].isActive === true & person[i].gender === "female"){
+      // console.log(person[i])
+  }
+}
+// person age > 30 atau dibawah 25 eyecolor blue
+// person with w 
+// inactive male age >25
+for (i=0; i<person.length; i++){
+  let arr = []
+  if( person[i].gender === "male" & person[i].age > 25){
+      // console.log(person[i])
+  }
+}
+// age < 30 atau eye color brown banana
+
+// male
+for (i=0; i<person.length; i++){
+  let arr = []
+  if(person[i].gender === "male"){
+      // console.log(person[i])
+  }
+}
+// blue
+for (i=0; i<person.length; i++){
+  let arr = []
+  if(person[i].eyeColor === "blue"){
+      // console.log(person[i])
+  }
+}
+// age > 30
+for (i=0; i<person.length; i++){
+  let arr = []
+  if(person[i].age > 30){
+      //  console.log(person[i].name)
+  }
+}
